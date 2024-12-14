c80746b9bbf5dadf2edd8cb2bd9fdba63fa6a5d5
testlab <testmail@ow.com>
2024-12-14 06:10:40 +0000

    update provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = "my-project-id"
+  project     = "GCP_Project01"
   region      = "us-central1"
   credentials = file("/home/devendra/gcp/base/platinum-logic-426017-v3-ebb1ab63c365.json")
 }
